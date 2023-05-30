0ad64cb7f00ec744b5767df75c0577caaf0e4861
Georg Jung <git@gjung.com>
2023-05-30 20:45:59 +0200

    Add rename-modelzoo-bin-jpegs command

--- src/FaceAiSharp.Validation/Program.cs
+++ src/FaceAiSharp.Validation/Program.cs
@@ -40,6 +40,10 @@ var threshold = new Option<float>(
     name: "--threshold",
     getDefaultValue: () => 0.29f);
 
+var binJpegs = new Option<DirectoryInfo>(
+    name: "--bin-jpegs");
+binJpegs.IsRequired = true;
+
 var generateEmbeddings = new Command("generate-embeddings") { dataset, arcfaceModel, scrfdModel, dbEmbeddingCollectionName, pairsFile };
 
 var calcAllDistances = new Command("calc-all-distances") { dbEmbeddingCollectionName, threshold };
@@ -47,6 +51,8 @@ var calcAllDistances = new Command("calc-all-distances") { dbEmbeddingCollection
 var calcPairsDistances = new Command("calc-pairs-distances") { dbEmbeddingCollectionName, threshold, pairsFile };
 
 var countClosedEyes = new Command("count-closed-eyes") { dataset, scrfdModel, eyeStateModel };
+
+var renameModelzooBinJpegs = new Command("rename-modelzoo-bin-jpegs") { binJpegs, pairsFile };
 
 #pragma warning disable SA1116 // Split parameters should start on line after declaration
 #pragma warning disable SA1117 // Parameters should be on same line or separate lines
@@ -78,5 +84,12 @@ countClosedEyes.SetHandler((dataset, db, scrfdModel, eyeStateModel) =>
     cnt.Invoke();
 }, dataset, db, scrfdModel, eyeStateModel);
 rc.AddCommand(countClosedEyes);
+
+renameModelzooBinJpegs.SetHandler((binJpegs, pairsFile) =>
+{
+    var ren = new RenameModelzooBinJpegs(binJpegs, pairsFile);
+    ren.Invoke();
+}, binJpegs, pairsFile);
+rc.AddCommand(renameModelzooBinJpegs);
 
 return await rc.InvokeAsync(args);
---
+++ src/FaceAiSharp.Validation/RenameModelzooBinJpegs.cs
@@ -0,0 +1,52 @@
+// Copyright (c) Georg Jung. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.CommandLine;
+using System.Data;
+using System.Diagnostics;
+using System.Threading.Channels;
+using FaceAiSharp.Abstractions;
+using FaceAiSharp.Extensions;
+using LiteDB;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Microsoft.ML.OnnxRuntime;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace FaceAiSharp.Validation;
+
+internal sealed class RenameModelzooBinJpegs
+{
+    private readonly DirectoryInfo _binJpegs;
+    private readonly FileInfo _pairsFile;
+
+    public RenameModelzooBinJpegs(DirectoryInfo binJpegs, FileInfo pairsFile)
+    {
+        _binJpegs = binJpegs;
+        _pairsFile = pairsFile;
+    }
+
+    public void Invoke()
+    {
+        var fld = _binJpegs.FullName;
+        var pairs = Dataset.ParsePairs(_pairsFile.FullName).SelectMany(x => x);
+        var cnt = 0;
+        foreach (var defPair in pairs)
+        {
+            var leftFile = Path.Combine(fld, $"{cnt * 2:D5}.jpg");
+            var rightFile = Path.Combine(fld, $"{cnt * 2 + 1:D5}.jpg");
+            var fldLeft = Path.Combine(fld, defPair.Identity1);
+            string id2 = defPair.SameIdentity ? defPair.Identity1 : defPair.Identity2!;
+            var fldRight = Path.Combine(fld, id2);
+            Directory.CreateDirectory(fldLeft);
+            Directory.CreateDirectory(fldRight);
+
+            var leftNew = Path.Combine(fldLeft, defPair.Identity1 + $"_{defPair.ImageNumber1:D4}.jpg");
+            var rightNew = Path.Combine(fldRight, id2 + $"_{defPair.ImageNumber2:D4}.jpg");;
+            File.Move(leftFile, leftNew, true);
+            File.Move(rightFile, rightNew, true);
+            cnt++;
+        }
+    }
+}
